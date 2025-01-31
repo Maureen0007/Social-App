91ddc427ada773aab5ba78dcab78d24ccb553846
Maureen <maureenwanjiku85@outlook.com>
2025-01-31 15:17:05 +0300

    add and style the post/feed section

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,15 @@ import "./rightbar.css"
 export default function Rightbar() {
   return (
     <div className="rightbar">
-        Rightbar
+        <div className="rightbarWrapper">
+          <div className="birthdayContainer">
+            <img src="assets/gift.png" alt="" className="birthdayImg" />
+            <span className="birthdayText">
+              <b>Alice Wanda</b> and <b>4 other friends</b> have a birthday today
+            </span>
+          </div>
+          <img src="assets/ad.png" alt="" className="rightbarAd" />
+        </div>
         </div>
   )
 }
